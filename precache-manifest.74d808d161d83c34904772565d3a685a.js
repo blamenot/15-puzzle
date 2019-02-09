88fe9ebcffdef9141a114a57ac7d41ae805c360a
Denis <dgushcha@oooinex.ru>
2019-02-09 19:24:37 +0300

    Updates

---
+++ precache-manifest.74d808d161d83c34904772565d3a685a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cd9072dea3cc0fd7e26",
+    "url": "/15-puzzle/static/css/main.30f8cada.chunk.css"
+  },
+  {
+    "revision": "9cd9072dea3cc0fd7e26",
+    "url": "/15-puzzle/static/js/main.9cd9072d.chunk.js"
+  },
+  {
+    "revision": "24dba00d2c0dfced1fd3",
+    "url": "/15-puzzle/static/js/1.24dba00d.chunk.js"
+  },
+  {
+    "revision": "6078883dbe333bf38113",
+    "url": "/15-puzzle/static/js/runtime~main.6078883d.js"
+  },
+  {
+    "revision": "7ab39efcb875b206e7ec6c74768b7513",
+    "url": "/15-puzzle/index.html"
+  }
+];
